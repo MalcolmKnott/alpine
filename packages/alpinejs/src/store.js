9393c6ecc76e587ca1b0042b7e553957c49dbe8c
Malcolm Knott <malcolm.knott@gmail.com>
2021-09-23 12:18:42 +1200

    WIP

--- packages/alpinejs/src/store.js
+++ packages/alpinejs/src/store.js
@@ -1,10 +1,20 @@
 import { reactive } from "./reactivity"
+import { persist } from "../../persist/src/index"
+import { interceptor, initInterceptors } from './interceptor'
+
 
 let stores = {}
 let isReactive = false
 
 export function store(name, value) {
+
     if (! isReactive) { stores = reactive(stores); isReactive = true; }
+
+    if (typeof value === 'function') {
+        value = value.bind({
+                '$persist': persist(null, {Alpine, interceptor})
+            })()
+    }
 
     if (value === undefined) {
         return stores[name]
